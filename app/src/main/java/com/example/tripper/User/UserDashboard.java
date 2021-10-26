3397785e1e50d2feb69ce03c36c0e822a7666072
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2021-10-27 02:09:50 +0530

    Featured Location

--- app/src/main/java/com/example/tripper/User/UserDashboard.java
+++ app/src/main/java/com/example/tripper/User/UserDashboard.java
@@ -1,13 +1,23 @@
 package com.example.tripper.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
+import com.example.tripper.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.example.tripper.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.example.tripper.R;
 
+import java.util.ArrayList;
+
 public class UserDashboard extends AppCompatActivity {
+
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +25,23 @@ public class UserDashboard extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
 
+        featuredRecycler = findViewById(R.id.featured_recycler);
+
+        featuredRecycler();
     }
+
+    private void featuredRecycler() {
+
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations=new ArrayList<>();
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali,"Manali","Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali,"Manali","Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali,"Manali","Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+    }
+
 }
