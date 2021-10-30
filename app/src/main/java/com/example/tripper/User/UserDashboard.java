f7572988b9f38a688915fde9c8949d0877971c9e
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2021-10-30 14:23:21 +0530

    Navigation Drawer Added

--- app/src/main/java/com/example/tripper/User/UserDashboard.java
+++ app/src/main/java/com/example/tripper/User/UserDashboard.java
@@ -1,13 +1,18 @@
 package com.example.tripper.User;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tripper.HelperClasses.HomeAdapter.CategoriesAdapter;
 import com.example.tripper.HelperClasses.HomeAdapter.CategoriesHelperClass;
@@ -16,42 +21,79 @@ import com.example.tripper.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.example.tripper.HelperClasses.HomeAdapter.MostViewedAdapter;
 import com.example.tripper.HelperClasses.HomeAdapter.MostViewedHelperClass;
 import com.example.tripper.R;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class UserDashboard extends AppCompatActivity {
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    RecyclerView featuredRecycler,mostViewedRecycler,categoriesRecycler;
+    //Variables
+    RecyclerView featuredRecycler, mostViewedRecycler, categoriesRecycler;
     RecyclerView.Adapter adapter;
-    GradientDrawable gradient1,gradient2,gradient3,gradient4;
+    GradientDrawable gradient1, gradient2, gradient3, gradient4, gradient5;
+    ImageView menuIcon;
+
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
 
         featuredRecycler = findViewById(R.id.featured_recycler);
-        categoriesRecycler=findViewById(R.id.categories_recycler);
-        mostViewedRecycler=findViewById(R.id.most_viewed_recycler);
+        categoriesRecycler = findViewById(R.id.categories_recycler);
+        mostViewedRecycler = findViewById(R.id.most_viewed_recycler);
+        menuIcon = findViewById(R.id.menu_icon);
+
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+        navigationDrawer();
+
 
         //Function Execute when dashboard created
         featuredRecycler();
         mostViewedRecycler();
-        categoriesRecycler();;
+        categoriesRecycler();
+        ;
+    }
+
+    private void navigationDrawer() {
+        //Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
     }
 
     private void mostViewedRecycler() {
         mostViewedRecycler.setHasFixedSize(true);
         mostViewedRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        ArrayList<MostViewedHelperClass> mostViewedLocation=new ArrayList<>();
-        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali,"Manali"));
-        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali,"manali"));
-        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali,"manali"));
-        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali,"manali"));
+        ArrayList<MostViewedHelperClass> mostViewedLocation = new ArrayList<>();
+        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali, "Manali"));
+        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali, "Manali"));
+        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali, "Manali"));
+        mostViewedLocation.add(new MostViewedHelperClass(R.drawable.manali, "Manali"));
 
-        adapter=new MostViewedAdapter(mostViewedLocation);
+        adapter = new MostViewedAdapter(mostViewedLocation);
         mostViewedRecycler.setAdapter(adapter);
 
     }
@@ -59,12 +101,12 @@ public class UserDashboard extends AppCompatActivity {
     private void featuredRecycler() {
 
         featuredRecycler.setHasFixedSize(true);
-        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        ArrayList<FeaturedHelperClass> featuredLocations=new ArrayList<>();
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali,"Manali","Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali,"Manali","Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
-        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali,"Manali","Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali, "Manali", "Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali, "Manali", "Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.manali, "Manali", "Manali offers the most magnificent views of the Pir Panjal and the Dhauladhar ranges covered with snow"));
 
         adapter = new FeaturedAdapter(featuredLocations);
         featuredRecycler.setAdapter(adapter);
@@ -72,23 +114,28 @@ public class UserDashboard extends AppCompatActivity {
 
     }
 
-    private void categoriesRecycler(){
-        gradient1 =new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,new int[]{0xffeff400,0xffaff600});
-        gradient2 =new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,new int[]{0xff7adccf,0xff7adccf});
-        gradient3 =new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,new int[]{0xff7c59f,0xFF7c59f});
-        gradient4 =new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,new int[]{0xffb8d7f5,0xffb8d7f5});
-
-        ArrayList<CategoriesHelperClass> categoriesHelperClasses=new ArrayList<>();
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1,R.drawable.restaurant_logo,"Restaurant"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1,R.drawable.restaurant_logo,"Restaurant"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1,R.drawable.restaurant_logo,"Restaurant"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1,R.drawable.restaurant_logo,"Restaurant"));
-        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1,R.drawable.restaurant_logo,"Restaurant"));
+    private void categoriesRecycler() {
+        gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xff8360c3, 0XFF2ebf91});
+        gradient2 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xff1f4037, 0xff99f2c8});
+        gradient3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xff7F7FD5, 0xFF86A8E7, 0xff91EAE4});
+        gradient4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffc31432, 0xff240b36});
+        gradient5 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xff659999, 0xfff4791f});
+        ArrayList<CategoriesHelperClass> categoriesHelperClasses = new ArrayList<>();
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1, R.drawable.restaurant_logo, "Restaurant"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient2, R.drawable.hotles_icon, "Hotels"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient3, R.drawable.shop_icon, "Shops"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient4, R.drawable.airport_icon, "Airports"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient5, R.drawable.restaurant_logo, "Restaurant"));
 
         categoriesRecycler.setHasFixedSize(true);
-        adapter=new CategoriesAdapter(categoriesHelperClasses);
-        categoriesRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        adapter = new CategoriesAdapter(categoriesHelperClasses);
+        categoriesRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         categoriesRecycler.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+
+    }
 }
