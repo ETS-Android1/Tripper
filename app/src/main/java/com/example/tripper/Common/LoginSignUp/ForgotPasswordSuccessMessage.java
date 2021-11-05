5c3087bdecb2a1ce2bf8d07e1121f7ec33c299ab
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2021-11-05 15:30:21 +0530

    ForgotPassword Added

---
+++ app/src/main/java/com/example/tripper/Common/LoginSignUp/ForgotPasswordSuccessMessage.java
@@ -0,0 +1,27 @@
+package com.example.tripper.Common.LoginSignUp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tripper.R;
+
+public class ForgotPasswordSuccessMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_success_message);
+    }
+
+    public void callBackScreen(View view) {
+        ForgotPasswordSuccessMessage.super.onBackPressed();
+    }
+
+    public void callLoginScreen(View view) {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/tripper/Common/LoginSignUp/SetNewPassword.java
+++ app/src/main/java/com/example/tripper/Common/LoginSignUp/SetNewPassword.java
@@ -3,20 +3,14 @@ package com.example.tripper.Common.LoginSignUp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.tripper.R;
 
-public class MakeSelection extends AppCompatActivity {
+public class SetNewPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_make_selection);
-    }
-
-
-    public void callBackScreen(View view){
-        MakeSelection.super.onBackPressed();
+        setContentView(R.layout.activity_set_new_password);
     }
 }
