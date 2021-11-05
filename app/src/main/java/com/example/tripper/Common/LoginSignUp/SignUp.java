aa629bff5b33ea01d7f7391f012019e6c7c223e2
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2021-11-06 01:11:24 +0530

    Login Validation

--- app/src/main/java/com/example/tripper/Common/LoginSignUp/SignUp.java
+++ app/src/main/java/com/example/tripper/Common/LoginSignUp/SignUp.java
@@ -1,28 +1,28 @@
 package com.example.tripper.Common.LoginSignUp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Pair;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tripper.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUp extends AppCompatActivity {
 
     ImageView backBtn;
+
+    //variables
+    TextInputLayout fullName, userName, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_contributor_sign_up);
 
-        backBtn=findViewById(R.id.signup_back_btn);
+        backBtn = findViewById(R.id.signup_back_btn);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,16 +30,107 @@ public class SignUp extends AppCompatActivity {
                 SignUp.super.onBackPressed();
             }
         });
+
+        //Hooks for getting data
+        fullName = findViewById(R.id.signup_fullname);
+        userName = findViewById(R.id.signup_username);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+
+    }
+
+
+    private boolean validateFullName() {
+        String value = fullName.getEditText().getText().toString().trim();
+
+        if (value.isEmpty()) {
+            fullName.setError("Field cannot be empty");
+            return false;
+        } else {
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        String value = userName.getEditText().getText().toString().trim();
+        String RegularExpression = "\\A\\w{1,20}\\z";
+
+        if (value.isEmpty()) {
+            userName.setError("Field cannot be empty");
+            return false;
+        } else if (value.length() > 20) {
+            userName.setError("Username should be between 1-20");
+            return false;
+        } else if (!value.matches(RegularExpression)) {
+            userName.setError("No white spaces allowed");
+            return false;
+        } else {
+            userName.setError(null);
+            userName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        String value = email.getEditText().getText().toString().trim();
+        String RegularExpression = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        if (value.isEmpty()) {
+            email.setError("Field cannot be empty");
+            return false;
+        } else if (!value.matches(RegularExpression)) {
+            email.setError("Email is invalid");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+
+    private boolean validatePassword() {
+        String value = password.getEditText().getText().toString().trim();
+        String RegularExpression = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{4,}$";
+
+        if (value.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else if (!value.matches(RegularExpression)) {
+            password.setError("Password should contain 1 digit, 1 special character and should have 4 letters.");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+    public void callNextSignUpScreen(View view) {
+
+        if(!validateFullName() | !validateUsername() | !validateEmail() | !validatePassword()){
+            return;
+        }
+
+        Intent intent = new Intent(getApplicationContext(), SignUp2ndClass.class);
+        intent.putExtra("fullName",fullName.getEditText().getText().toString().trim());
+        intent.putExtra("Username",userName.getEditText().getText().toString().trim());
+        intent.putExtra("Email",email.getEditText().getText().toString().trim());
+        intent.putExtra("Password",password.getEditText().getText().toString().trim());
+        startActivity(intent);
     }
 
 
 
-    public void callNextSignUpScreen(View view){
-        Intent intent=new Intent(getApplicationContext(),SignUp2ndClass.class);
-            startActivity(intent);
-        }
-        public  void callLoginScreen(View view){
-            Intent intent=new Intent(getApplicationContext(),Login.class);
-            startActivity(intent);
-        }
+
+    public void callLoginScreen(View view) {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/tripper/Common/LoginSignUp/SignUp2ndClass.java
+++ app/src/main/java/com/example/tripper/Common/LoginSignUp/SignUp2ndClass.java
@@ -3,22 +3,34 @@ package com.example.tripper.Common.LoginSignUp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tripper.R;
 
+import java.util.Calendar;
+
 public class SignUp2ndClass extends AppCompatActivity {
 
     ImageView backBtn;
+    RadioGroup radioGroup;
+    DatePicker datePicker;
+    RadioButton selectedGender;
 
+    String fullName,username,email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up2nd_class);
 
         backBtn = findViewById(R.id.signup_back_btn);
+        radioGroup = findViewById(R.id.radio_group);
+        datePicker = findViewById(R.id.age_picker);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,14 +38,62 @@ public class SignUp2ndClass extends AppCompatActivity {
                 SignUp2ndClass.super.onBackPressed();
             }
         });
+
+        Intent intent=getIntent();
+        fullName=intent.getStringExtra("fullName");
+        username=intent.getStringExtra("Username");
+        email=intent.getStringExtra("Email");
+        password=intent.getStringExtra("Password");
+
+    }
+
+    private boolean validateGender() {
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateAge() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int userYear = datePicker.getYear();
+        int difference = currentYear - userYear;
+
+        if (difference < 14) {
+            Toast.makeText(this, "Age should be more than 14years", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public void callNextSignUpScreen(View view) {
+
+        if (!validateAge() | !validateGender()) {
+            return;
+        }
+
+        selectedGender = findViewById(radioGroup.getCheckedRadioButtonId());
+        String gender = selectedGender.getText().toString();
+
+        int day=datePicker.getDayOfMonth();
+        int month=datePicker.getMonth();
+        int year=datePicker.getYear();
+
+        String date=day+"/"+month+"/"+year;
         Intent intent = new Intent(getApplicationContext(), SignUp3rdClass.class);
+        intent.putExtra("gender",gender);
+        intent.putExtra("dob",date);
+        intent.putExtra("fullName",fullName);
+        intent.putExtra("username",username);
+        intent.putExtra("email",email);
+        intent.putExtra("password",password);
         startActivity(intent);
     }
 
-    public void callBackScreen(View view){
+    public void callBackScreen(View view) {
         SignUp2ndClass.super.onBackPressed();
     }
 }
