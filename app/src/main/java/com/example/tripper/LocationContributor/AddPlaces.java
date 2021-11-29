95981c3bc039725d920fd2e7706aa5c3d3db528c
Harsh <kumarharsh8549@gmail.com>
2021-11-30 00:37:59 +0530

    Place Catalogue

--- app/src/main/java/com/example/tripper/LocationContributor/AddPlaces.java
+++ app/src/main/java/com/example/tripper/LocationContributor/AddPlaces.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -29,6 +31,8 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -38,8 +42,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class AddPlaces extends AppCompatActivity {
 
     private CircleImageView Imagepicker;
-    private ArrayList<Uri> imageUris;
-    private ArrayList<String> urlStrings;
+    private Uri imageUris;
+    private String urlStrings;
     private ProgressDialog progressDialog;
     private static final int PICK_IMAGES_CODES = 0;
     private TextInputLayout title, description, categories, bestVisitTime, budget, address;
@@ -61,7 +65,6 @@ public class AddPlaces extends AppCompatActivity {
         bestVisitTime = findViewById(R.id.addplace_bestVisitTime);
         budget = findViewById(R.id.addplace_budget);
         address = findViewById(R.id.addplace_address);
-        imageUris = new ArrayList<>();
 
         //progress dialog
         progressDialog = new ProgressDialog(this);
@@ -159,28 +162,21 @@ public class AddPlaces extends AppCompatActivity {
         progressDialog.show();
 
         timestamp = "" + System.currentTimeMillis();
-        urlStrings = new ArrayList<>();
 
-        if (imageUris.size() == 0) {
-            Toast.makeText(this, "Atleast one phone should be uploaded", Toast.LENGTH_SHORT).show();
+        if (imageUris == null) {
+            Toast.makeText(this, "One photo should be uploaded", Toast.LENGTH_SHORT).show();
             return;
         } else {
-            int image_count;
             String filePathName="place_images/"+""+timestamp;
             StorageReference storageReference= FirebaseStorage.getInstance().getReference(filePathName);
-
-            for(image_count=0;image_count<imageUris.size();image_count++){
-                Uri individualImage=imageUris.get(image_count);
+                Uri individualImage=imageUris;
                 storageReference.putFile(individualImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
-                                urlStrings.add(String.valueOf(uri));
-                                if (urlStrings.size() == imageUris.size()){
-                                    storeLink(urlStrings);
-                                }
+                                urlStrings=String.valueOf(uri);
                             }
                         });
                     }
@@ -205,12 +201,14 @@ public class AddPlaces extends AppCompatActivity {
             hashMap.put("placeBudget", "" + placeBudget);
             hashMap.put("placeAddress", "" + placeAddress);
             hashMap.put("userId", "" +phone);
+            hashMap.put("placeImage",""+urlStrings);
 
             DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
             reference.child(phone).child("Places").child(timestamp).setValue(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void unused) {
                     Toast.makeText(getApplicationContext(),"Place added Successfully",Toast.LENGTH_LONG).show();
+                    progressDialog.dismiss();
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -222,65 +220,16 @@ public class AddPlaces extends AppCompatActivity {
 
         }
 
-    }
 
-    private void storeLink(ArrayList<String> urlStrings) {
-
-        HashMap<String, String> hashMap = new HashMap<>();
-
-        for (int i = 0; i <urlStrings.size() ; i++) {
-            hashMap.put("ImgLink"+i, urlStrings.get(i));
-
-        }
-
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
-        databaseReference.child(phone).child("Places").child(timestamp).child("PlaceImg").setValue(hashMap)
-                .addOnCompleteListener(
-                        new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Successfully Uploaded", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }
-                ).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-        progressDialog.dismiss();
-
-        imageUris.clear();
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // When an Image is picked
-        if (requestCode == PICK_IMAGES_CODES && resultCode == RESULT_OK && null != data) {
-            // Get the Image from data
-            if (data.getClipData() != null) {
-                ClipData mClipData = data.getClipData();
-                int cout = data.getClipData().getItemCount();
-                for (int i = 0; i < cout; i++) {
-                    // adding imageuri in array
-                    Uri imageurl = data.getClipData().getItemAt(i).getUri();
-                    imageUris.add(imageurl);
-                }
-                // setting 1st selected image into image switcher
-                Imagepicker.setImageURI(imageUris.get(0));
-                position = 0;
-            } else {
-                Uri imageurl = data.getData();
-                imageUris.add(imageurl);
-                Imagepicker.setImageURI(imageUris.get(0));
-                position = 0;
-            }
-        } else {
-            // show this if no image is selected
-            Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        if(requestCode == PICK_IMAGES_CODES && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            imageUris = data.getData();
+            Imagepicker.setImageURI(imageUris);
         }
     }
 }
