4e2b4cf5360d8fdfba5a45a445de429a15d9b069
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2022-01-20 01:15:08 +0530

    Rating and Review System Added

--- app/src/main/java/com/example/tripper/User/Destination.java
+++ app/src/main/java/com/example/tripper/User/Destination.java
@@ -2,10 +2,16 @@ package com.example.tripper.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Pair;
 import android.widget.TextView;
 
+import com.example.tripper.HelperClasses.rating.BarLabels;
+import com.example.tripper.HelperClasses.rating.RatingReviews;
 import com.example.tripper.R;
+
+import java.util.Random;
 
 public class Destination extends AppCompatActivity {
 
@@ -18,5 +24,31 @@ public class Destination extends AppCompatActivity {
         placeDisplayId=findViewById(R.id.placeDisplyId);
         placeId=getIntent().getStringExtra("placeId");
         placeDisplayId.setText(placeId);
+
+        RatingReviews ratingReviews = (RatingReviews) findViewById(R.id.rating_reviews);
+
+        Pair colors[] = new Pair[]{
+                new Pair<>(Color.parseColor("#0c96c7"), Color.parseColor("#00fe77")),
+                new Pair<>(Color.parseColor("#7b0ab4"), Color.parseColor("#ff069c")),
+                new Pair<>(Color.parseColor("#fe6522"), Color.parseColor("#fdd116")),
+                new Pair<>(Color.parseColor("#104bff"), Color.parseColor("#67cef6")),
+                new Pair<>(Color.parseColor("#ff5d9b"), Color.parseColor("#ffaa69"))
+        };
+
+        int minValue = 30;
+
+        int raters[] = new int[]{
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1),
+                minValue + new Random().nextInt(100 - minValue + 1)
+        };
+
+
+        ratingReviews.createRatingBars(100, BarLabels.STYPE3, colors, raters);
+
+
+
     }
 }
